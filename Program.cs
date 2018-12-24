0950df6b8a0bc9e8e4c6249be5e8b830c59c47a4
Vartika06 <magicalvartika06@gmail.com>
2018-12-24 13:03:55 +0530

    Made it interactive

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Your Name?");
+            var name = Console.ReadLine(); 
+            
+            Console.WriteLine("How many hours of sleep did you get last night?");
+            var hoursOfSleep = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hello " + name);
+            if (hoursOfSleep > 8)
+            {
+                Console.WriteLine("You are well rested");
+            }
+            else
+            {
+                Console.WriteLine("You need more Sleep.");
+            }
         }
     }
 }
